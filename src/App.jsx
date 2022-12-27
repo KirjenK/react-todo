9d527b6a8748740830346c4cb4901b3441450a1b
Kirill <kirpitch@inbox.ru>
2022-12-27 21:50:40 +0300

    package init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div className="App">
+      Hello!
+    </div>
+  );
+}
+
+export default App;
